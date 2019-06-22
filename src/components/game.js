adaf6cd1fb43f952a84104288dcc89b5bb865201
wabbot <wabbot24@gmail.com>
2019-06-22 09:44:10 -0400

    file structure

---
+++ src/components/game.js
@@ -0,0 +1,17 @@
+import Data from "../Data.json"
+// have this data in the state function
+import React from "react"
+import Component from "react"
+
+import Navbar from "./navbar"
+
+// import images from ./image-components/all-the-images
+
+// send props to navbar, which then sends props to click-result
+
+// use componentDidMount() to do the setState() that shuffles all the images
+//   - this is attached to the class syntax and does not need to be called
+
+// onclick event will set state
+
+//i'm changing this to see if that makes a difference
