433984d4cf13e65a679f835eff42f0cc8f14a0bd
wabbot <wabbot24@gmail.com>
2019-06-29 11:55:47 -0400

    Updates

---
+++ precache-manifest.40b96cdaf96c76f2e080cb1da0090a64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3f09a1c7fa445641668675257ae5a0e",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "3a8bc00bc41be3924859",
+    "url": "/memory-game/static/css/main.31f7c387.chunk.css"
+  },
+  {
+    "revision": "bdc426c3d81b3cc97d92",
+    "url": "/memory-game/static/js/2.8fa3d35f.chunk.js"
+  },
+  {
+    "revision": "3a8bc00bc41be3924859",
+    "url": "/memory-game/static/js/main.cefbcc99.chunk.js"
+  },
+  {
+    "revision": "799512f57ffe7b17c145",
+    "url": "/memory-game/static/js/runtime~main.4bfb86a8.js"
+  }
+]);
